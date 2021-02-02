e69f2428b4d7079ac3da8cce809384b0a8223025
katsuyama akiyoshi <katsuyama@gitlab.com>
2021-02-02 20:56:22 +0900

    feat: local実行

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,15 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 
 import {NodejsFunction} from '@aws-cdk/aws-lambda-nodejs';
+import * as apigateway from '@aws-cdk/aws-apigateway'
+
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const fnDemo = new NodejsFunction(this, 'demo', {
+    const createUserLambda = new NodejsFunction(this, 'createUserLambda', {
       entry: 'src/lambda/handlers/createUserLambda.ts',
     });
-  }
 
+    const LambdaRestAPI = new apigateway.RestApi(this, 'LambdaRestApi', {
+      restApiName:"LambdaRestApi",
+    })
+
+    LambdaRestAPI.root.addResource('todo').addMethod('POST', new apigateway.LambdaIntegration(createUserLambda))
+
+
+  }
 }
--- backend/src/lambda/handlers/createUserLambda.ts
+++ backend/src/lambda/handlers/createUserLambda.ts
@@ -1,36 +1,39 @@
+import { APIGatewayEventRequestContext, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
 import UserRepository from "../infrastructure/inmemory-user-repository"
 import { CreateUser } from "../usecases/createUser"
 
-export async function handler(event: any, context: any): Promise<any> {
+export async function handler(
+    event: APIGatewayProxyEvent,
+    context: APIGatewayEventRequestContext
+): Promise<APIGatewayProxyResult> {
 
-    const createuser=new CreateUser(new UserRepository())
-    try{
-        const result=await createuser.execute(1,"test")
-    }catch(e){
-        console.log(e)
-        let msg=""
-        if (e instanceof Error){
-            msg="error"
+    const createuser = new CreateUser(new UserRepository())
+    try {
+        const result = await createuser.execute(1, "test")
+    } catch (e) {
+
+        let msg = ""
+        if (e instanceof Error) {
+            msg = "error"
         }
-        else{
-            msg="default"
+        else {
+            msg = "default"
         }
 
         return {
-            statusCode:500,
-            body:JSON.stringify({
-                message:e.name
+            statusCode: 500,
+            body: JSON.stringify({
+                message: e.name
             })
         }
     }
 
     return {
         statusCode: 201,
+        body: JSON.stringify({
+            status: "ok",
+            error: ""
+        })
     }
 }
-
-handler({},{}).then((data:any)=>{
-    console.log(data)
-}).catch((e:any)=>{
-    console.log(e)
-})
